d9a32bf2ab166d4556485baa71a1422d884a218d
Gabriel Musskopf <52297777+gabrielmusskopf@users.noreply.github.com>
2022-08-15 20:36:02 -0300

    feat: is numero primo

---
+++ algoritimos-e-programacao/is-numero-primo.py
@@ -0,0 +1,15 @@
+def isPrimo(num):
+  if num == 1:
+    return True
+
+  aux = 2
+  while aux < 10:
+    if aux != num and num % aux == 0:
+      return False
+    aux += 1
+  return True
+
+while True:
+  n = int(input("Digite um número: "))
+  print("É primo.\n" if isPrimo(n) else "Não é primo.\n")
+  
