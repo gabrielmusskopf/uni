6819ec78b6c27e38ee9a5ff1a8474f9047a69fca
Gabriel Musskopf <gabriel.musskopf@cwi.com.br>
2023-08-28 22:00:19 -0300

    sync notes

--- fundamentos-de-sistemas-operacionais/gerencia-filas.c
+++ fundamentos-de-sistemas-operacionais/gerencia-filas.c
@@ -7,18 +7,20 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void cria_processo();
-void define_info();
+int luck(int s, int e) {
+    return (rand()%e) + s;
+}
 
-#define MAX_PROCESS 10 /* Quantidade máxima de processos que o sistema suporta */
+#define MAX_PROCESS 10 
 
 typedef struct DescProc {
     char     estado_atual;
     int      prioridade;
     unsigned inicio_memoria;
     unsigned tamanho_memoria;
-    //struct   Arquivos arquivos_abertos[20];
     unsigned tempo_cpu;
     unsigned proc_pc;
     unsigned proc_sp;
@@ -32,39 +34,157 @@ typedef struct DescProcLista {
     DescProc* ultimo;
 } DescProcLista;
 
+void cria_processo();
+void define_info(DescProc* p);
 
-DescProc tab_desc[MAX_PROCESS]; /* Quantidade máxima permitida de processos criados no sistema */
+DescProc tab_desc[MAX_PROCESS]; 
 DescProcLista desc_livre;
 DescProcLista espera_cpu;
 DescProcLista usando_cpu;
 DescProcLista bloqueados;
 
-void define_info(DescProc* processo) { /* cria o processo inserindo informações em seu PCB */
-    printf("Voce define aqui as informacoes... :-)\n");
-    /* Definir os dados do processo aqui */
+void show_process(DescProc *p) {
+    printf("\nestado atual: %c\n", p->estado_atual);
+    printf("prioridade: %d\n", p->prioridade);
+    printf("inicio memoria: %u\n", p->inicio_memoria);
+    printf("tamanho memoria: %u\n", p->tamanho_memoria);
+    printf("tempo cpu: %u\n", p->tempo_cpu);
+    printf("pc: %u\n", p->proc_pc);
+    printf("sp: %u\n", p->proc_sp);
+    printf("acc: %u\n", p->proc_acc);
+    printf("rx: %u\n", p->proc_rx);
+}
+
+void show_queue(DescProcLista *lista) {
+    DescProc *p = lista->primeiro;
+    DescProc *aux = p;
+
+    while (aux) {
+        show_process(aux);
+        printf("\n");
+        aux = aux->proximo;
+    }
+}
+
+void show_state(char *title){
+    printf("--- %s ---\n", title);
+    printf("< Espera CPU: > [");
+    show_queue(&espera_cpu);
+    printf("]\n");
+
+    printf("< Usando: > [");
+    show_queue(&usando_cpu);
+    printf("]\n");
+
+    printf("< Bloqueado: > [");
+    show_queue(&bloqueados);
+    printf("]\n");
+    printf("--------------------\n\n");
+}
+
+void define_info(DescProc* p) { 
+    p->estado_atual= 'A';
+    p->prioridade = luck(1, 100);
+    p->inicio_memoria = 10;
+    p->tamanho_memoria = 10;
+    p->tempo_cpu = 10;
+    p->proc_pc = 12;
+    p->proc_rx = 11;
+    p->proc_acc= 11;
+    p->proc_sp= 11;
+}
+
+void add_to_queue(DescProc *p, DescProcLista *l) {
+    if(l->primeiro == NULL) {
+        l->primeiro = l->ultimo = p;
+        p->proximo = NULL;
+    } else {
+        l->ultimo->proximo = p;
+        l->ultimo = p;
+        p->proximo = NULL;
+    }
+}
+
+DescProc *remove_first(DescProcLista *l) {
+    if (l->primeiro == NULL) {
+        return NULL;
+    }
+    DescProc *p = l->primeiro;
+    l->primeiro = l->primeiro->proximo;
+    return p;
+}
+
+void remove_last(DescProcLista *l) {
+    if (l->primeiro == l->ultimo) {
+        l->primeiro= NULL;
+    }
+    l->ultimo = NULL;
 }
 
 void cria_processo() {
-    DescProc* processo = desc_livre.primeiro;
+    DescProc* p = desc_livre.primeiro;
+
     printf("Definindo informacoes do processo (PCB)\n");
-    define_info(processo);
+    define_info(p);
 
     printf("Atualizando a fila desc_livre... O processo em execucao esta usando um dos descritores.\n");
     DescProc* proximo_livre = desc_livre.primeiro->proximo;
     desc_livre.primeiro = proximo_livre;
-    printf("Processo criado... migramos ele para a fila espera_cpu.\n");
 
-    if(espera_cpu.primeiro == NULL) {
-        espera_cpu.primeiro = espera_cpu.ultimo = processo;
-    } else {
-        espera_cpu.ultimo->proximo = processo;
-        espera_cpu.ultimo = processo;
-        processo->proximo = NULL;
+    printf("Processo criado... migramos ele para a fila espera_cpu.\n");
+    add_to_queue(p, &espera_cpu);
+
+}
+
+void cpu_bound() {
+    DescProc *p = remove_first(&espera_cpu);
+    if (p == NULL) {
+        printf("Nada mais a executar!\n");
+        return;
+    }
+
+    add_to_queue(p, &usando_cpu);
+    show_state("Processo com CPU");
+    remove_last(&usando_cpu);
+
+    if (luck(1, 10) <= 5) {
+        printf("Processo finalizado com sucesso!\n");
+        return;
+    }
+
+    add_to_queue(p, &espera_cpu);
+    show_state("Tempo do processo esgotado, volta para a fila de espera");
+}
+
+void io_bound() {
+    DescProc *p = remove_first(&espera_cpu);
+    if (p == NULL) {
+        printf("Nada mais a executar!\n\n");
+        return;
+    }
+
+    printf("Processo ganhou CPU\n");
+    add_to_queue(p, &bloqueados);
+    show_state("Processo iniciou IO bound e foi bloqueado");
+
+    remove_last(&bloqueados);
+    add_to_queue(p, &espera_cpu);
+    show_state("Processo finalizou IO bound e está aguardando CPU novamente");
+}
+
+void do_work() {
+    printf("CPU trabalhando...\n");
+
+    while (espera_cpu.primeiro != NULL) {
+        cpu_bound();
+        io_bound();
+        show_state("Ciclo finalizado");
     }
 }
 
-
 int main() {
+    srand(time(NULL));
+
     int i;
 
     printf("Organizando a estrutura de descritores disponiveis no sistema operacional.\n");
@@ -86,7 +206,15 @@ int main() {
     bloqueados.ultimo   = NULL;
     
     printf("Criando o primeiro processo...\n");
+
     cria_processo();
+    cria_processo();
+
+    show_state("Antes de iniciar");
+
+    do_work();
+
+    show_state("Após finalizado");
 
     printf("Done!\n");
     return 0;
